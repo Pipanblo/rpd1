718dd8dffdca5561899964b81417db5c0e32cf14
Pipablo <bayezid.dil@gmail.com>
2023-05-16 13:04:34 +0300

    created checkout page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-
-import Button from "../button/button.component";
 import { CartContext } from "../../contexts/cart.context";
+import Button from "../button/button.component";
+
 import "./product-card.styles.scss";
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -11,6 +11,7 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
+    
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
@@ -18,8 +19,8 @@ const ProductCard = ({ product }) => {
         <span className="price"> {price} </span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
-        {" "}
-        Sepete Ekle{" "}
+        
+        Sepete Ekle
       </Button>
     </div>
   );
